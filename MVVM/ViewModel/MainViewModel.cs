59ea0139bee8ff83403633edbe735b017c142236
Mattthew Prokofev <mattthew.pr@gmail.com>
2022-09-07 20:41:49 +0300

    -fix commands in MainViewModel

--- MVVM/ViewModel/MainViewModel.cs
+++ MVVM/ViewModel/MainViewModel.cs
@@ -1,26 +1,11 @@
 ﻿using BankingAppMVVM.Core;
+using BankingAppMVVM.Core.Commands;
+using System.Windows.Input;
 
 namespace BankingAppMVVM.MVVM.ViewModel
 {
     internal class MainViewModel : ViewModelBase
     {
-        #region Command fields
-        public Command ShowAccountsView { get; set; }
-        public Command ShowSettingsView { get; set; }
-        public Command ShowNewAccountView { get; set; }
-        public Command ShowAboutView { get; set; }
-
-        #endregion
-
-        #region View fields
-
-        public AccountsViewModel AccountsVM { get; set; }
-        public SettingsViewModel SettingsVM { get; set; }
-        public NewAccountViewModel NewAccountVM { get; set; }
-        public AboutViewModel AboutVM { get; set; }
-
-        #endregion
-
         #region Current View
 
         private object _currentView;
@@ -33,6 +18,63 @@ namespace BankingAppMVVM.MVVM.ViewModel
                 OnPropertyChanged();
             }
         }
+
+        #endregion
+
+        #region View fields
+
+        public AccountsViewModel AccountsVM { get; set; }
+        public SettingsViewModel SettingsVM { get; set; }
+        public NewAccountViewModel NewAccountVM { get; set; }
+        public AboutViewModel AboutVM { get; set; }
+
+        #endregion
+
+        #region Commands
+
+        #region ShowAccountsView
+
+        public ICommand ShowAccountsView { get; }
+        private bool CanShowAccountsViewExecute(object p) => true;
+        private void OnShowAccountsViewExecuted(object p)
+        {
+            CurrentView = AccountsVM;
+        }
+
+        #endregion
+
+        #region ShowSettingsView
+
+        public ICommand ShowSettingsView { get; }
+        private bool CanShowSettingsViewExecute(object p) => true;
+        private void OnShowSettingsViewExecuted(object p)
+        {
+            CurrentView = SettingsVM;
+        }
+
+        #endregion
+
+        #region ShowSettingsView
+
+        public ICommand ShowNewAccountView { get; }
+        private bool CanShowNewAccountViewExecute(object p) => true;
+        private void OnShowNewAccountViewExecuted(object p)
+        {
+            CurrentView = NewAccountVM;
+        }
+
+        #endregion
+
+        #region ShowAboutView
+
+        public ICommand ShowAboutView { get; }
+        private bool CanShowAboutViewExecute(object p) => true;
+        private void OnShowAboutViewExecuted(object p)
+        {
+            CurrentView = AboutVM;
+        }
+
+        #endregion
 
         #endregion
 
@@ -49,17 +91,12 @@ namespace BankingAppMVVM.MVVM.ViewModel
 
             #endregion
 
-            #region Toolbar comands
+            #region Change view commands
 
-
-            #endregion
-
-            #region Change view
-
-            //ShowAccountsView = new Command(o => { CurrentView = AccountsVM; });
-            //ShowSettingsView = new Command(o => { CurrentView = SettingsVM; });
-            //ShowNewAccountView = new Command(o => { CurrentView = NewAccountVM; });
-            //ShowAboutView = new Command(o => { CurrentView = AboutVM; });
+            ShowAccountsView = new RelayCommand(OnShowAccountsViewExecuted, CanShowAccountsViewExecute);
+            ShowSettingsView = new RelayCommand(OnShowSettingsViewExecuted, CanShowSettingsViewExecute);
+            ShowNewAccountView = new RelayCommand(OnShowNewAccountViewExecuted, CanShowNewAccountViewExecute);
+            ShowAboutView = new RelayCommand(OnShowAboutViewExecuted, CanShowAboutViewExecute);
 
             #endregion
         }
