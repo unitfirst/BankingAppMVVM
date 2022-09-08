7da2411bf773e37eb43abdd73609759eda85d418
Mattthew Prokofev <mattthew.pr@gmail.com>
2022-09-08 21:43:11 +0300

    -add AccessValidator

---
+++ MVVM/Model/Access/Parameters/EditAccess.cs
@@ -0,0 +1,11 @@
+﻿namespace BankingAppMVVM.MVVM.Model.Access.Parameters
+{
+    public class EditAccess
+    {
+        public bool FirstName;
+        public bool LastName;
+        public bool MidleName;
+        public bool PhoneNumber;
+        public bool PassortData;
+    }
+}
